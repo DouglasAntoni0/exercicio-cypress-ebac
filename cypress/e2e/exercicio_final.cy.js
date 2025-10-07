2d9d62ada4eddd7b3b25f69764cea72b32ab593d
Douglas Antonio <douglas.tony1235@gmail.com>
2025-10-07 20:39:48 -0300

    teste corrigido

--- cypress/e2e/exercicio_final.cy.js
+++ cypress/e2e/exercicio_final.cy.js
@@ -2,40 +2,54 @@ import { faker } from '@faker-js/faker';
 
 describe('Exercício Final - Testes de ponta a ponta', () => {
 
-    it('Deve realizar o cadastro, adicionar um produto ao carrinho e validar', () => {
+    it('Deve realizar o cadastro, adicionar produto, cadastrar endereço e finalizar a compra', () => {
         cy.intercept('PUT', '**/public/updateCart/**').as('updateCartApi');
         cy.setCookie('ebacStoreVersion', 'v2'); 
         cy.visit('http://lojaebac.ebaconline.art.br/'); 
 
+        // --- Geração de Dados Dinâmicos ---
         const nome = faker.person.firstName();
         const sobrenome = faker.person.lastName();
-        const telefone = faker.phone.number('## #####-####'); 
+        const telefone = '33999653200'; // Usando número fixo
         const email = faker.internet.email({ firstName: nome });
         const senha = faker.internet.password({ length: 10 }); 
 
-        
+        // --- Fluxo de Cadastro ---
         cy.get('[href="/Tab/Account"]').click();
         cy.get('[data-testid="signUp"]').click();
-
         cy.get('[data-testid="firstName"]').type(nome);
         cy.get('[data-testid="lastName"]').type(sobrenome);
         cy.get('[data-testid="phone"]').type(telefone);
-        cy.get(':nth-child(7) > .css-175oi2r > [data-testid="email"]').type(email);
-        cy.get(':nth-child(8) > .css-175oi2r > [data-testid="password"]').type(senha);
+        cy.get('[data-testid="email"]').eq(1).type(email);
+        cy.get('[data-testid="password"]').eq(1).type(senha);
         cy.get('[data-testid="repassword"]').type(senha); 
-
         cy.get('[data-testid="create"]').click();
 
-        cy.get('[style="color: rgb(255, 255, 255); font-size: 20px; font-family: Montserrat-Bold;"]').should('contain', 'EBAC Store');
-        
-        cy.wait(2000); 
-
-        
-
-        cy.get(':nth-child(28) > .r-1nteefp > .r-1awozwy').click();
-        cy.get('[style="margin: 5px 7px; border-color: rgb(128, 128, 128); border-width: 0.5px; border-radius: 15px; width: 445px;"] > [data-testid="productDetails"]').click();
+        // --- Adicionar Produto ao Carrinho ---
+        cy.contains('CAT-PROD-1758926136180').click();
+        cy.contains('Tuna').click();
         cy.get('[data-testid="addToCart"]').click();
         cy.wait('@updateCartApi');
+
+        // --- Cadastrar Novo Endereço ---
         cy.contains('My Cart').should('be.visible');
+        cy.get('[data-testid="addNewAddress"]').click();
+        cy.get('input[placeholder="Enter your name"]').type(`${nome} ${sobrenome}`);
+        cy.get('input[placeholder="Enter your mobile number"]').type(telefone);
+        cy.get('input[placeholder="Enter your address"]').type(faker.location.streetAddress());
+        cy.get('input[placeholder="City"]').type(faker.location.city());
+        cy.get('input[placeholder="State"]').type(faker.location.state());
+        cy.get('input[placeholder="ZipCode"]').type(faker.location.zipCode());
+        cy.get('[data-testid="save"]').click();
+        cy.contains(`${nome} ${sobrenome}`).should('be.visible');
+
+        // --- Finalizar a Compra ---
+        cy.get('[data-testid="selectAddressOrContinueToPayment"]').click();
+        cy.get('[data-testid="completeCheckout"]').click();
+
+        // --- Validação Final do Pedido ---
+
+        cy.contains('Order Success').should('be.visible');
+        cy.get('[data-testid="goBackHome"]').should('be.visible');
     });
 });
